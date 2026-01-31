f79ea8798f65bd9c1898d37a1de1d29c1a06fbe9
kahwooi <kahwooi@msn.com>
2026-01-31 09:53:50 +0800

    remove frontend and restructure the project

--- frontend/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-// src/global.d.ts
-declare module '*.jpg' {
-  const value: string;
-  export default value;
-}
-declare module '*.png';
-declare module '*.svg';
-declare module '*.gif';
-declare module '*.webp';
